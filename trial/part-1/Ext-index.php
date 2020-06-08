5702ecd87f3eeb847352b7386bfa54bdf4c594f5
Chalid Ade Rahman <chalidade@gmail.com>
2020-06-08 07:39:36 +0700

    up

---
+++ trial/part-1/Ext-index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link href='../../extJs/build/modern/theme-material/resources/theme-material-all-debug.css' rel='stylesheet'/>
+    <script src='../../extJs/build/ext-modern-all.js'></script>
+    <script src='Modified-app.js'></script>
+  </head>
+  <body>
+
+  </body>
+</html>
